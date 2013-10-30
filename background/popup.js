9622605158678582387a7567b6fe95bd9560e137
pastak <pasta0915@gmail.com>
2013-10-30 11:50:29 +0900

    フォントサイズ変更

--- background/popup.js
+++ background/popup.js
@@ -4,6 +4,18 @@ $(function(){
         var txt = $('#txt').prop('value');
         window.clickEval();
     });
+    var fontSizeSelector = $('#fontSize').on('change', function(event){
+                var value = $(this).prop('value');
+                $('textarea').css({fontSize : (value+'px')});
+                localStorage.setItem('chroSchemeFontSize',value);
+            })
+;
+    for(var i = 10; i <= 90; i++){
+        fontSizeSelector.append(
+            $('<option>').text((i+' px')).prop('value',i).prop('selected',(localStorage.getItem('chroSchemeFontSize') == i))
+        );
+    }
+    $('textarea').css({fontSize : ((localStorage.getItem('chroSchemeFontSize')||'10')+'px')});
     $('#clearInputButton').on('click',function(){
         $('txt').prop('value','');
     });
